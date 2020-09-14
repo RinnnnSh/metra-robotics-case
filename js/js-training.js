87394c68c4a87a450b4f4636d603bd00f0cbfee4
RinnnnSh <64162999+RinnnnSh@users.noreply.github.com>
2020-09-14 17:40:08 +0300

    gg

--- js/js-training.js
+++ js/js-training.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
   $('.project__title').click(
     function() {
-      $('.project__title').css("color", "rgb(" + randomColor() + ", " + randomColor() + ", " + randomColor() + ")");
+      $(this).css("color", "rgb(" + randomColor() + ", " + randomColor() + ", " + randomColor() + ")");
   });
 
   function randomColor() {
@@ -16,17 +16,28 @@ $(document).ready(function () {
       $('.project__frame-item:last-child').toggleClass('project__frame-item--order1');
   })
 
-
-  $('.project__wrap-item img').click(
-    function() {
-      $('.project__wrap-item img').animate({
-        "position": "relative",
-        "left": "100%"
-
-      },"slow")
+  $('.project__wrap-item img').mouseenter(function() {
+    $(this).animate({ deg: 360 }, {duration: 1200, step: function(now) {
+          $(this).css({ transform: 'rotate(' + now + 'deg)' });
+        }
+      }
+    );
+    $(this).animate({left:'100vw'},1200);
   });
 
+  $('.project__wrap-item img').mouseleave(
+    function() {
+      $(this).animate({ deg: 0 }, {duration: 10, step: function(now) {
+            $(this).css({ transform: 'rotate(' + now + 'deg)' });
+          }
+        }
+      );
+      $(this).animate({left:'0'},10);
+  });
 
+  $('.js-feedback-form').submit(function() {
+    $(this).attr('action', 'http://metrarobotics.ru/endpoints/mail2.php')
+  });
 
 
 
